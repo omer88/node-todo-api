d0082075d1b0536e5c3a6ae70c79a96ef8561140
Omer Levy <omer88@users.noreply.github.com>
2018-06-11 16:59:21 +0300

    Preparation for a deployment

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const { mongoose } = require('./db/mongoose');
 
-app.listen(3000, () => {
-  console.log('listening to port 3000');
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
 });
