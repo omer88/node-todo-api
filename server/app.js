c1cfa1749dc5fd7caaba9650c84ede4870616856
Omer Levy <omer88@users.noreply.github.com>
2018-06-11 15:33:55 +0300

    Add GET /todos/:id route

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { Todo } = require('./models/Todo');
 const { User } = require('./models/User');
@@ -23,6 +24,22 @@ app.get('/todos', async (req, res) => {
     res.send({ todos });
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+    }
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo });
+  } catch (error) {
+    res.status(400).send();
   }
 });
 module.exports = app;
